1cdd1dda8c93e71165c59a3375b7387b93e0bb19
aydansn <97697518+aydansn@users.noreply.github.com>
2024-07-25 20:36:57 +0400

    Add files via upload

---
+++ WeatherDashboard/script.js
@@ -0,0 +1,11 @@
+const toggleSwitch = document.getElementById('modeToggle');
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+}
